a9d2bcc58ac3b95d22d5c0682b8b718feb130563
chenjiahui-xxz <1768861061@qq.com>
2023-06-04 22:00:54 +0800

    hshsh

---
+++ common.go
@@ -0,0 +1,12 @@
+package go_common
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello, world!")
+}
+
+// Add 函数将两个整数相加并返回结果
+func Add(a, b int) int {
+	return a + b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chenjiahui-xxz/go-common
+
+go 1.16
